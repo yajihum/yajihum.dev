cc4cfa3c05090f3c5b66763a5fa9e1933f2f5d43
yajium <yajiuma720@gmail.com>
2023-10-24 23:57:01 +0900

    fix: cloudflare kv

--- workers/src/index.ts
+++ workers/src/index.ts
@@ -1,7 +1,8 @@
 export interface Env {
-	MY_KV: KVNamespace;
+	MY_KV_STAMP: KVNamespace;
 }
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
 		const { pathname } = new URL(request.url);
@@ -9,15 +10,21 @@ export default {
 		switch (request.method) {
 			case 'GET':
 				const key = pathname.slice(1);
-				const value = await env.MY_KV.get(key);
+				const value = await env.MY_KV_STAMP.get(key);
 				if (value === null) {
 					return new Response('Value not found', { status: 404 });
 				}
 				return new Response(value);
 			case 'PUT':
-				const newValue = await request.text();
-				await env.MY_KV.put(pathname.slice(1), newValue);
-				return new Response('Success', { status: 200 });
+				const emojiUrl = await request.text();
+				const emojiList = await env.MY_KV_STAMP.get(pathname.slice(1));
+				if (emojiList === null) {
+					await env.MY_KV_STAMP.put(pathname.slice(1), JSON.stringify([emojiUrl]));
+				} else {
+					await env.MY_KV_STAMP.put(pathname.slice(1), JSON.stringify([...JSON.parse(emojiList), emojiUrl]));
+				}
+				const newEmojiList = await env.MY_KV_STAMP.get(pathname.slice(1));
+				return new Response(newEmojiList);
 			default:
 				return new Response(`${request.method} is not allowed.`, {
 					status: 405,
