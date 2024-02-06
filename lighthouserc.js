c0e6c13fa9b9551cbf1308a7692b2c925f808004
yajium <yajiuma720@gmail.com>
2024-02-06 22:47:24 +0900

    fix

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,9 +1,10 @@
 module.exports = {
 	ci: {
 		collect: {
-			startServerCommand: "pnpm start",
-			url: ["http://localhost:3000/"],
+			// startServerCommand: "pnpm start",
+			// url: ["http://localhost:3000/"],
 			numberOfRuns: 1,
+			staticDistDir: "./build",
 		},
 		upload: {
 			target: "temporary-public-storage",
