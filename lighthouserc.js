450b7760cf843b46a94641047761012e8a0253f6
yajium <yajiuma720@gmail.com>
2024-02-06 23:12:57 +0900

    fix

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,11 +3,14 @@ module.exports = {
 		collect: {
 			startServerCommand: "pnpm start",
 			url: [
-				"http://localhost:3000/",
-				"http://localhost:3000/blog/",
-				"http://localhost:3000/blog/posts/life/20231230_reflection/",
+				"http://localhost:3000",
+				"http://localhost:3000/blog",
+				"http://localhost:3000/blog/posts/life/20231230_reflection",
 			],
 			numberOfRuns: 1,
+			settings: {
+				emulatedFormFactor: "desktop",
+			},
 		},
 		upload: {
 			target: "temporary-public-storage",
