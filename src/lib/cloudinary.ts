1e0e5f30205ee8cdc30cf0e7be6c70bbd2067c5f
yajium <yajiuma720@gmail.com>
2024-02-24 18:55:49 +0900

    fix: og image design

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -1,4 +1,4 @@
 export const getOgpImageUrl = (title: string) => {
-  const url = `https://res.cloudinary.com/dpgixnkg1/image/upload/l_text:noto-snans.ttf_70_bold:${title},co_rgb:fff,w_1000,c_fit,y_-70/v1707824334/ogp-image.png`;
+  const url = `https://res.cloudinary.com/dpgixnkg1/image/upload/l_text:noto-snans.ttf_60_bold:${title},co_rgb:fff,w_1000,c_fit,y_-70/v1708768266/blog-ogp.png`;
   return url;
 };
