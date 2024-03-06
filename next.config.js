1afa6ab3de282d92ca8c8c74797af12ec959a4f5
yajium <yajiuma720@gmail.com>
2024-03-06 23:59:49 +0900

    fix: remove not-found data img

--- next.config.js
+++ next.config.js
@@ -1,24 +1,25 @@
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "images.site.yajihum.dev",
-				port: "",
-				pathname: "/**",
-			},
-			{
-				protocol: "https",
-				hostname: "cdn.emoji.yajihum.dev",
-				port: "",
-				pathname: "/**",
-			},
-		],
-	},
-	experimental: {
-		serverActions: true,
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.site.yajihum.dev',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.emoji.yajihum.dev',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
-module.exports = nextConfig;
+module.exports = withBundleAnalyzer(nextConfig);
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,15 +1,13 @@
 import { LinkToPage } from '@/components/atoms/LinkToPage';
-import { SadButRelievedFace } from '@/components/icons/SadButRelievedFace';
 
 export default async function NotFound() {
   return (
-    <section className="grid grid-cols-1 justify-items-center gap-10 py-10">
-      <h1 className="text-4xl font-bold">404 Not Found</h1>
-      <p className="text-sm md:text-2xl">
+    <section className='grid grid-cols-1 justify-items-center gap-10 py-10'>
+      <h1 className='text-4xl font-bold'>404 Not Found</h1>
+      <p className='text-sm md:text-2xl'>
         Sorry, the page you are looking for cannot be found.
       </p>
-      <LinkToPage href="/" title="Back to home page" />
-      <SadButRelievedFace className="w-200 h-200" />
+      <LinkToPage href='/' title='Back to home page' />
     </section>
   );
 }
