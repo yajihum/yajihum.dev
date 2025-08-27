3bdea1579da96af4128fd44ec8bb3fa8d69715c8
yajium <yajiuma720@gmail.com>
2025-08-28 00:23:28 +0900

    test

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -25,14 +25,13 @@ export async function generateMetadata(): Promise<Metadata> {
 
 export default async function PhotosPage() {
   const photos = getAllPhotos();
-  const sortedPhotos = photos.sort().reverse(); // 新しい順に並び替え
 
   return (
     <div className="grid grid-cols-1 gap-10">
       <h1 className="text-3xl">Photos</h1>
 
       <div className="grid grid-cols-3 gap-1">
-        {sortedPhotos.map((photo) => (
+        {photos.slice(0, 10).map((photo) => (
           <PhotoWithModal key={photo} photo={photo} />
         ))}
       </div>
