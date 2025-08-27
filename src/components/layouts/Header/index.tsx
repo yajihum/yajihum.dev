9ab8cc1a9a531d3759c2c03e93d5473a6675ec8f
yajium <yajiuma720@gmail.com>
2025-08-28 01:41:07 +0900

    fix: design

--- src/components/layouts/Header/index.tsx
+++ src/components/layouts/Header/index.tsx
@@ -6,7 +6,7 @@ export default async function Header() {
     <header className="sticky top-0 z-10 border-b border-stone-800 bg-neutral-900 py-2 transition-colors">
       <div className="mx-auto flex max-w-3xl items-center justify-between px-4 md:px-0">
         <Link href="/" className="flex items-center gap-1.5">
-          <h2 className="text-lg text-neutral-400 hover:text-neutral-200 md:text-xl">
+          <h2 className="text-lg text-neutral-300 hover:text-neutral-200 md:text-xl">
             yajihum.dev
           </h2>
         </Link>
